58d1d750c0e646b5edc52db420a8007763cb3816
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2022-06-02 13:05:12 +0100

    initial commit

---
+++ src/main/java/com/obamax/WalletUserOnboarding/WalletUserOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.obamax.WalletUserOnboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletUserOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletUserOnboardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/obamax/WalletUserOnboarding/WalletUserOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obamax.WalletUserOnboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletUserOnboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
