15fdba739a8287eec1d3c4ef3b66ccc1f8fafa57
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2022-06-08 16:50:14 +0100

    created fetchWalletBalance and fundWallet endpoints

--- src/main/java/com/obamax/WalletUserOnboarding/controllers/WalletController.java
+++ src/main/java/com/obamax/WalletUserOnboarding/controllers/WalletController.java
@@ -1,10 +1,31 @@
 package com.obamax.WalletUserOnboarding.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.obamax.WalletUserOnboarding.payload.responses.WalletResponse;
+import com.obamax.WalletUserOnboarding.services.WalletService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/wallets")
 public class WalletController {
 
+    private final WalletService walletService;
+
+    public WalletController(WalletService walletService) {
+        this.walletService = walletService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<WalletResponse> fetchWalletBalance(@PathVariable Long id) {
+        WalletResponse walletResponse = walletService.fetchWalletBalance(id);
+        return new ResponseEntity<>(walletResponse, HttpStatus.OK);
+    }
+
+    @PostMapping("/fund/{id}")
+    public ResponseEntity<WalletResponse> fundWallet(@PathVariable Long id, @RequestParam Double amount) {
+
+        WalletResponse walletResponse = walletService.fundWalletByWalletId(id, amount);
+        return new ResponseEntity<>(walletResponse, HttpStatus.OK);
+    }
 }
