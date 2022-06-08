721257024f20f10b3011f0455d8caa63a611cb3c
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2022-06-08 21:54:49 +0100

    implemented WalletWithdrawal method

--- src/main/java/com/obamax/WalletUserOnboarding/services/implementaion/WalletServiceImpl.java
+++ src/main/java/com/obamax/WalletUserOnboarding/services/implementaion/WalletServiceImpl.java
@@ -2,8 +2,15 @@ package com.obamax.WalletUserOnboarding.services.implementaion;
 
 import com.obamax.WalletUserOnboarding.exceptions.BadRequestException;
 import com.obamax.WalletUserOnboarding.exceptions.ResourceNotFoundException;
+import com.obamax.WalletUserOnboarding.models.Transaction;
 import com.obamax.WalletUserOnboarding.models.Wallet;
+import com.obamax.WalletUserOnboarding.models.enums.TransactionMode;
+import com.obamax.WalletUserOnboarding.models.enums.TransactionStatus;
+import com.obamax.WalletUserOnboarding.models.enums.TransactionType;
+import com.obamax.WalletUserOnboarding.payload.requests.WalletWithdrawalRequest;
 import com.obamax.WalletUserOnboarding.payload.responses.WalletResponse;
+import com.obamax.WalletUserOnboarding.payload.responses.WalletWithdrawalResponse;
+import com.obamax.WalletUserOnboarding.repositories.TransactionRepository;
 import com.obamax.WalletUserOnboarding.repositories.WalletRepository;
 import com.obamax.WalletUserOnboarding.services.WalletService;
 import org.springframework.stereotype.Service;
@@ -11,10 +18,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class WalletServiceImpl implements WalletService {
 
+    private static final String NOT_APPLICABLE = "NOT_APPLICABLE";
     private final WalletRepository walletRepository;
+    private final TransactionRepository transactionRepository;
 
-    public WalletServiceImpl(WalletRepository walletRepository) {
+    public WalletServiceImpl(WalletRepository walletRepository, TransactionRepository transactionRepository) {
         this.walletRepository = walletRepository;
+        this.transactionRepository = transactionRepository;
     }
 
     @Override
@@ -25,14 +35,59 @@ public class WalletServiceImpl implements WalletService {
 
     @Override
     public WalletResponse fundWalletByWalletId(Long id, Double amount) {
-
-        if (amount <= 0) throw new BadRequestException("Amount cannot be less than or equal to zero.");
-
         Wallet wallet = getWalletById(id);
+
+        if (amount <= 0) {
+            saveTransactionDetails(amount, wallet, TransactionMode.CREDIT, TransactionStatus.FAILED,
+                    TransactionType.FUND, NOT_APPLICABLE, NOT_APPLICABLE, NOT_APPLICABLE);
+            throw new BadRequestException("Amount cannot be less than or equal to zero.");
+        }
 
         wallet.setBalance(wallet.getBalance() + amount);
 
+        saveTransactionDetails(amount, wallet, TransactionMode.CREDIT, TransactionStatus.SUCCESSFUL,
+                TransactionType.FUND, NOT_APPLICABLE, NOT_APPLICABLE, NOT_APPLICABLE);
+
         return getWalletResponse(walletRepository.save(wallet));
+    }
+
+    @Override
+    public WalletWithdrawalResponse walletWithdrawal(WalletWithdrawalRequest walletWithdrawalRequest) {
+        Wallet wallet = getWalletById(walletWithdrawalRequest.getWalletId());
+        Double amount = walletWithdrawalRequest.getAmount();
+
+        if (amount <= 0) {
+            saveTransactionDetails(amount, wallet, TransactionMode.DEBIT, TransactionStatus.FAILED,
+                    TransactionType.WITHDRAW, walletWithdrawalRequest.getBankName(),
+                    walletWithdrawalRequest.getBeneficiaryAccountName(),
+                    walletWithdrawalRequest.getBeneficiaryAccountNumber());
+            throw new BadRequestException("Amount cannot be less than or equal to zero.");
+        }
+
+        if (wallet.getBalance() < amount) {
+            saveTransactionDetails(amount, wallet, TransactionMode.DEBIT, TransactionStatus.INSUFFICIENT_FUNDS,
+                    TransactionType.WITHDRAW, walletWithdrawalRequest.getBankName(),
+                    walletWithdrawalRequest.getBeneficiaryAccountName(),
+                    walletWithdrawalRequest.getBeneficiaryAccountNumber());
+            throw new BadRequestException("Insufficient funds.");
+        }
+
+        wallet.setBalance(wallet.getBalance() - amount);
+
+        Wallet savedWallet = walletRepository.save(wallet);
+
+        saveTransactionDetails(amount, wallet, TransactionMode.DEBIT, TransactionStatus.SUCCESSFUL,
+                TransactionType.WITHDRAW, walletWithdrawalRequest.getBankName(),
+                walletWithdrawalRequest.getBeneficiaryAccountName(),
+                walletWithdrawalRequest.getBeneficiaryAccountNumber());
+
+        return WalletWithdrawalResponse.builder()
+                .message("Withdrawal successful.")
+                .walletId(savedWallet.getId())
+                .bankName(walletWithdrawalRequest.getBankName())
+                .beneficiaryAccountNumber(walletWithdrawalRequest.getBeneficiaryAccountNumber())
+                .amount(amount)
+                .build();
     }
 
     private Wallet getWalletById(Long id) {
@@ -48,5 +103,16 @@ public class WalletServiceImpl implements WalletService {
                 .accountName(wallet.getUser().getName())
                 .balance(wallet.getBalance())
                 .build();
+    }
+
+    private void saveTransactionDetails(Double amount, Wallet wallet, TransactionMode transactionMode,
+                                        TransactionStatus transactionStatus, TransactionType transactionType,
+                                        String bankName, String beneficiaryAccountName,
+                                        String beneficiaryAccountNumber) {
+        transactionRepository.save(Transaction.builder().wallet(wallet).transactionMode(transactionMode)
+                .transactionStatus(transactionStatus).transactionType(transactionType).amount(amount)
+                .bankName(bankName).beneficiaryAccountName(beneficiaryAccountName)
+                .beneficiaryAccountNumber(beneficiaryAccountNumber)
+                .build());
     }
 }
