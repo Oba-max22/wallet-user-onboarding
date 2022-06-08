c2b1b1defe6e1493e675af47b81023d7177c16d4
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2022-06-08 16:49:04 +0100

    create fetchWalletBalance and fundWalletByWalletId methods

--- src/main/java/com/obamax/WalletUserOnboarding/services/WalletService.java
+++ src/main/java/com/obamax/WalletUserOnboarding/services/WalletService.java
@@ -1,4 +1,9 @@
 package com.obamax.WalletUserOnboarding.services;
 
+import com.obamax.WalletUserOnboarding.payload.responses.WalletResponse;
+
 public interface WalletService {
+    WalletResponse fetchWalletBalance(Long id);
+
+    WalletResponse fundWalletByWalletId(Long id, Double amount);
 }
